fffb7f29545aca039af143b841f188b3f4bcaa09
Tomi <olaoluwaakomolafe08@gmail.com>
2026-09-23 12:36:52 +0100

    add homepage

---
+++ tailwind/Tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', 'sans-serif'],
+      },
+      colors: {
+        brand: {
+          brown: '#543A23',
+          orange: '#F38B00',
+          cream: '#FBF9F5',
+          surface: '#F5F2EB',
+          muted: '#8C857B',
+          dark: '#1F1E1D',
+          border: '#E7E3DC',
+          sage: '#3A5A40'
+        }
+      }
+    }
+  }
+}
